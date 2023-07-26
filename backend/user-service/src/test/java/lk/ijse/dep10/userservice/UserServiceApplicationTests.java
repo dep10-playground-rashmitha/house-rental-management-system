92aaa3ba62eb5dc739494a05d582169d965d10a8
Rashmitha Piyushan <rashmithapiyushansl@gmail.com>
2023-07-26 17:13:31 +0530

    create user service project

---
+++ backend/user-service/src/test/java/lk/ijse/dep10/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep10.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
